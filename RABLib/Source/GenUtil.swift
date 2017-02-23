a3be39adec9cf793ebaaedccc19bd09b0fccd6e0
RAB <riceandbytes@gmail.com>
2017-02-23 13:40:13 -0800

    clean

--- RABLib/Source/GenUtil.swift
+++ RABLib/Source/GenUtil.swift
@@ -298,56 +298,6 @@ extension GenUtil {
     }
 }
 
-// MARK: - Image
-
-extension GenUtil {
-//    public class func contentTypeForImageData(data: NSData) -> String? {
-//        var c: Int = 0
-//        data.getBytes(&c, length: 1)
-//        
-//        switch c {
-//        case 0xFF:
-//            return "image/jpeg"
-//        case 0x89:
-//            return "image/png"
-//        case 0x47:
-//            return "image/gif"
-//        case 0x49:
-//            fallthrough
-//        case 0x4D:
-//            return "image/tiff"
-//        default:
-//            return nil
-//        }
-//    }
-    
-    /**
-    Find image type from NSData
-    http://stackoverflow.com/questions/29644168/get-image-file-type-programmatically-in-swift
-    
-    - parameter imgData: <#imgData description#>
-    
-    - returns: <#return value description#>
-    */
-    public class func contentTypeForImageData(_ imgData : Data) -> String? {
-        var c = [UInt8](repeating: 0, count: 1)
-        (imgData as NSData).getBytes(&c, length: 1)
-        let ext : String
-        switch (c[0]) {
-        case 0xFF:
-            ext = "jpg"
-        case 0x89:
-            ext = "png"
-        case 0x47:
-            ext = "gif"
-        case 0x49, 0x4D :
-            ext = "tiff"
-        default:
-            return nil
-        }
-        return ext
-    }
-}
 // MARK: - Video
 
 extension GenUtil {
