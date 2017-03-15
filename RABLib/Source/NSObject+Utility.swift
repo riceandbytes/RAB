a0388c0de9d4851e701e7ea34b4b08cbe9423362
RAB <riceandbytes@gmail.com>
2017-03-14 23:49:35 -0700

    update

--- RABLib/Source/NSObject+Utility.swift
+++ RABLib/Source/NSObject+Utility.swift
@@ -9,6 +9,23 @@
 import Foundation
 import UIKit
 
+// MARK: - Associated Object
+public extension NSObject {
+    func setAssociatedObject(_ value: AnyObject?, associativeKey: UnsafeRawPointer, policy: objc_AssociationPolicy) {
+        if let valueAsAnyObject = value {
+            objc_setAssociatedObject(self, associativeKey, valueAsAnyObject, policy)
+        }
+    }
+    
+    func getAssociatedObject(_ associativeKey: UnsafeRawPointer) -> Any? {
+        guard let valueAsType = objc_getAssociatedObject(self, associativeKey) else {
+            return nil
+        }
+        return valueAsType
+    }
+}
+
+// MARK: - Path Names
 public extension NSObject {
     
     public class var dynamicClassFullName: String { return NSStringFromClass(self) }
