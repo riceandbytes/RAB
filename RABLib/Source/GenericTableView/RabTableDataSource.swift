f53311f36af6c882bbf20946df806d6411277e82
RAB <riceandbytes@gmail.com>
2017-03-28 15:21:44 -0700

    update

--- RABLib/Source/GenericTableView/RabTableDataSource.swift
+++ RABLib/Source/GenericTableView/RabTableDataSource.swift
@@ -82,23 +82,28 @@ open class RabTableDataSource {
         items.append(any)
     }
     
-    open subscript (index: Int) -> DataRow {
+    open subscript (index: Int) -> DataRow? {
+        guard items.containsIndex(index) else { return nil }
         return items[index]
     }
     
-    open func objectAtIndex(_ index: Int) -> DataRow {
+    open func objectAtIndex(_ index: Int) -> DataRow? {
+        guard items.containsIndex(index) else { return nil }
         return items[index]
     }
     
     open func remove(_ index: Int) {
+        guard items.count > 0 else { return }
         items.remove(at: index)
     }
     
     open func removeLastItem() {
+        guard items.count > 0 else { return }
         items.removeLast()
     }
     
     open func clearAllData() {
+        guard items.count > 0 else { return }
         items.removeAll()
     }
 }
