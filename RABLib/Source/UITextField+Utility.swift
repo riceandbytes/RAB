6553ac4519d53794c81d03f804f251668097dee6
bob <bob@helloworld.com>
2017-06-21 00:41:03 -0700

    up

--- RABLib/Source/UITextField+Utility.swift
+++ RABLib/Source/UITextField+Utility.swift
@@ -41,3 +41,19 @@ extension UITextField {
         self.layer.masksToBounds = true
     }
 }
+
+// MARK: - Add a bottom border
+
+extension UITextField {
+
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+}
