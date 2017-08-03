e6709739bcc342b63c80cc2ec5291f31f6cf6da4
ZeroWaitApp <zerowaitapp@gmail.com>
2017-08-03 10:18:00 -0700

    update

--- RABLib/Source/Float+Utility.swift
+++ RABLib/Source/Float+Utility.swift
@@ -17,7 +17,7 @@ import Foundation
  */
 extension Float {
     
-    var cleanValue: String {
+    public var cleanValue: String {
         return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
     }
 }
