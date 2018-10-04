fae77f8df806f3df6f3a334c10ffea8562ca615d
v <skylinefighterx@visvas-MacBook-Pro.local>
2018-10-03 22:17:49 -0700

    update

--- RABLib/Source/String+Utility.swift
+++ RABLib/Source/String+Utility.swift
@@ -146,7 +146,14 @@ extension String {
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
         return dateFormatter.date(from: self)
     }
-    
+    /// timeZoneId: example "America/New_York"
+    public func toDateISO8601Format(withTimeZone: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
+        dateFormatter.timeZone = TimeZone(identifier: withTimeZone)
+        return dateFormatter.date(from: self)
+    }
     public func toDateISO8601FormatGMT0() -> Date? {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
