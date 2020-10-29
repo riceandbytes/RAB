079ce7c1d48ac82d50330266cf54f34d711e3947
v <>
2020-10-28 17:56:49 -0700

    clean up

--- RABLib/Source/GenAnimateUtil.swift
+++ RABLib/Source/GenAnimateUtil.swift
@@ -8,24 +8,24 @@
 
 import Foundation
 
-open class GenAnimateUtil {
-    
-    /**
-     Usage:
-        Remember that you stil need to set the image
-        let fade = newCrossfade(..., ...)
-        myButton.imageView?.layer.add(fade, forKey: "animateContents")
-     
-        // still need to set this image afterwards
-        myButton.setImage(UIImage(named: "nameOfMyToImage"), forState: .Normal)
-     */
-    open class func newCrossfade(_ fromImage: UIImage?, toImage: UIImage?) -> CABasicAnimation {
-        let fade = CABasicAnimation(keyPath: "contents")
-        fade.duration = 0.7
-        fade.fromValue = fromImage
-        fade.toValue = toImage
-        fade.isRemovedOnCompletion = false
-        fade.fillMode = kCAFillModeForwards
-        return fade
-    }
-}
+//open class GenAnimateUtil {
+//
+//    /**
+//     Usage:
+//        Remember that you stil need to set the image
+//        let fade = newCrossfade(..., ...)
+//        myButton.imageView?.layer.add(fade, forKey: "animateContents")
+//
+//        // still need to set this image afterwards
+//        myButton.setImage(UIImage(named: "nameOfMyToImage"), forState: .Normal)
+//     */
+//    open class func newCrossfade(_ fromImage: UIImage?, toImage: UIImage?) -> CABasicAnimation {
+//        let fade = CABasicAnimation(keyPath: "contents")
+//        fade.duration = 0.7
+//        fade.fromValue = fromImage
+//        fade.toValue = toImage
+//        fade.isRemovedOnCompletion = false
+//        fade.fillMode = kCAFillModeForwards
+//        return fade
+//    }
+//}
